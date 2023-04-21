8411a29f3e400ec201c987e73eaf44a1fa02d60c
AnshulBanndhuCs <130958183+AnshulBanndhuCs@users.noreply.github.com>
2023-04-21 12:57:26 +0530

    Add files via upload

---
+++ Models/Books.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookList_Backend.Models.Models
+{
+    public class Books
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string author { get; set; }
+        public string image { get; set; }
+        public int genreId { get; set; }
+        public Genre genre { get; set; }
+    }
+}
---
+++ ViewModels/BookVM.cs
@@ -0,0 +1,24 @@
+﻿using BookList_Backend.Models.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookList_Backend.Models.ViewModels
+{
+    public class BookVM
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string author { get; set; }
+        public string image { get; set; }
+        //public Genre Genre { get; set; }
+        public int GenreId { get; set; }
+        //public string GenreName { get; set; }
+        //public Books Books { get; set; }
+        //public IEnumerable<SelectListItem> bookListItems { get; set; }
+    }
+}
